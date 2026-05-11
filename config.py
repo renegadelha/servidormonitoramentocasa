fd8703ea9b6ffe8d7e44a6ef31c8c812932dfe27
renegadelha <renegadelha@gmail.com>
2026-05-11 18:44:15 -0300

    fix-bug: funcao repet

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 
-placas_registradas = {'janela':'192.168.0.117', 'esp8266tu':'192.168.0.93', 'esp8266ar':'192.168.0.172','esp01luzesc':'192.168.0.195'}
+placas_registradas = {'janela':'192.168.0.117', 'esp8266tu':'192.168.0.93', 'esp8266ar':'192.168.0.172','esp01luzesc':'192.168.0.195','esp32c3vent':'192.168.0.166'}
 estado_quarto = {'dormir': 0, 'ar_ligado': 0 ,'janela_aberta': 1, 'temperatura_limite': 28}
 
